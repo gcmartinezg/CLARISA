ca98615e65e5e04025b81f7832fd51fdc7677359
jdmunozg <juan.david.m.g1004@gmail.com>
2023-08-18 09:07:10 -0500

    Feat(Install morgan )

--- toc-integration/src/index.ts
+++ toc-integration/src/index.ts
@@ -3,9 +3,11 @@ import cors from "cors";
 import Routes from "./routes";
 import {createConnection} from 'typeorm';
 import 'reflect-metadata';
+import morgan from 'morgan';
 
 const app = express();
 createConnection();
+app.use(morgan('dev'));
 app.set('port',3800)
 app.use(cors());
 app.use(express.json());
