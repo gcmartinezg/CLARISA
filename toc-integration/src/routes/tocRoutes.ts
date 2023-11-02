14bba30054cf2592b93c4e68b223ffaef9c877b2
Juan Delgado <juandelgadog98@gmail.com>
2023-11-02 08:43:24 -0500

    :recycle: refactor(ToC Integration): Updated validation and slack notification

--- toc-integration/src/routes/tocRoutes.ts
+++ toc-integration/src/routes/tocRoutes.ts
@@ -9,17 +9,21 @@ const TocResultDashboard = new tocController();
 router.post("/toc", TocResultDashboard.getTocResultDashboard);
 
 // Get test
-router.get("/tocs", TocResultDashboard.getToc)
+router.get("/tocs", TocResultDashboard.getToc);
 
-router.post('/test-slack-notificacion', (req, res) => {
-    try {
-        sendSlackNotification('INIT-01');
-        return res.status(200).json({ message: 'Notificación enviada' });
-    } catch (error) {
-        res.status(500).json({ error: 'Ha ocurrido un error' });
-    }
+router.post("/test-slack-notificacion", (req, res) => {
+  try {
+    sendSlackNotification(
+      ":alert:",
+      "INIT-01",
+      "A problem occurred while synchronizing with ToC"
+    );
+    return res.status(200).json({ message: "Notificación enviada" });
+  } catch (error) {
+    res.status(500).json({ error: "Ha ocurrido un error" });
+  }
 });
 
 // Get test
-router.get("/test", TocResultDashboard.getTest)
+router.get("/test", TocResultDashboard.getTest);
 export default router;
