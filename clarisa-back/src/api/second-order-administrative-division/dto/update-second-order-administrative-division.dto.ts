fbfdf5cb112b7b0bdb57216e2a37b4d1f0061bcb
German Martinez <g.martinez@cgiar.org>
2023-11-14 11:28:52 -0500

    ♻️refactor(subnational scope): deleting previous endpoints

--- clarisa-back/src/api/second-order-administrative-division/dto/update-second-order-administrative-division.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSecondOrderAdministrativeDivisionDto } from './create-second-order-administrative-division.dto';
-
-export class UpdateSecondOrderAdministrativeDivisionDto extends PartialType(
-  CreateSecondOrderAdministrativeDivisionDto,
-) {}
