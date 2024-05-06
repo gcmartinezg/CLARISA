fcfe670136727ede66f6acba71cfe5db82442856
yecksin <yecksin@gmail.com>
2024-05-06 12:17:03 -0500

    :sparkles: feat(Angular 17) P2-661: app.component added

--- bi-client/src/app/app.component.ts
+++ bi-client/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'powerBi';
-  constructor() {}
+  title = 'bi-client';
 }
